74767a92dd28ca196f1dccc5958444997046b18a
architectureman <111078057+architectureman@users.noreply.github.com>
2024-10-15 05:07:34 +0700

    Create macos_setup.sh

---
+++ macos_setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Add Homebrew to PATH
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Install Warp
+brew install --cask warp
+
+# Install Arc browser
+brew install --cask arc
+
+# Install Brave browser
+brew install --cask brave-browser
+
+#Install OKD
+brew install openshift-cli
+
+# Install Podman
+brew install podman
+
+# Install Podman Desktop
+brew install --cask podman-desktop
+
+# Install Docker
+brew install docker
+
