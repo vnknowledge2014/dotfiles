35b90530e8a3c2ea1a007eb255842eb66021cff1
architectureman <111078057+architectureman@users.noreply.github.com>
2024-10-15 05:07:58 +0700

    Create linux_setup.sh

---
+++ linux_setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Update and upgrade
+sudo apt update && sudo apt upgrade -y
+
+# Install essential build tools
+sudo apt install -y build-essential curl file git
+
+# Install OKD, Docker, Podman
+brew install docker podman openshift-cli 
+
+# Install Warp
+read -p "Do you want to install Warp? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    source ./scripts/install_warp.sh
+fi
+
+# Install Brave browser
+read -p "Do you want to install Brave browser? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sudo apt install apt-transport-https curl
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    sudo apt update
+    sudo apt install brave-browser
+fi
+
+# Install Podman Desktop
+read -p "Do you want to install Podman Desktop? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+     brew install --cask podman-desktop
+fi
