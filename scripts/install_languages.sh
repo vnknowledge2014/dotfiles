25fc81087059accc9defaadf6130ced6871330e8
architectureman <111078057+architectureman@users.noreply.github.com>
2024-10-15 05:14:11 +0700

    Create install_languages.sh

---
+++ scripts/install_languages.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+# Install Node.js
+asdf plugin add nodejs
+asdf install nodejs latest
+asdf global nodejs latest
+asdf shell nodejs latest
+
+# Install Bun
+asdf plugin add bun
+asdf install bun latest
+asdf global bun latest
+asdf shell bun latest
+
+# Install Deno
+asdf plugin add deno
+asdf install deno latest
+asdf global deno latest
+asdf shell deno latest
+
+# Install PureScript
+asdf plugin add purescript
+asdf install purescript latest
+asdf global purescript latest
+asdf shell purescript latest
+
+# Install Erlang
+asdf plugin add erlang
+asdf install erlang latest
+asdf global erlang latest
+asdf shell erlang latest
+
+# Install Elixir
+asdf plugin add elixir
+asdf install elixir latest
+asdf global elixir latest
+asdf shell elixir latest
+
+# Install Golang
+asdf plugin add golang
+asdf install golang latest
+asdf global golang latest
+asdf shell golang latest
+
+# Install Gleam
+asdf plugin add gleam
+asdf install gleam latest
+asdf global gleam latest
+asdf shell gleam latest
+
+# Install Lua
+asdf plugin add lua
+asdf install lua latest
+asdf global lua latest
+asdf shell lua latest
+
+# Install Java (Oracle)
+asdf plugin add java
+asdf install java oracle-17
+asdf global java oracle-17
+asdf shell java oracle-17
+
+# Install Zig
+asdf plugin add zig
+asdf install zig latest
+asdf global zig latest
+asdf shell zig latest
+
+# Install Rust
+asdf plugin add rust
+asdf install rust latest
+asdf global rust latest
+asdf shell rust latest
+
+# Install Ruby
+asdf plugin add ruby
+asdf install ruby latest
+asdf global ruby latest
+asdf shell ruby latest
+
+# Install Python
+asdf plugin add python
+asdf install python latest
+asdf global python latest
+asdf shell python latest
+
+# Install Unison
+asdf plugin add unison
+asdf install unison latest
+asdf global unison latest
+asdf shell unison latest
+
+# Install Haskell
+asdf plugin haskell unison
+asdf install haskell latest
+asdf global haskell latest
+asdf shell haskell latest
+
+# Install Kotlin
+asdf plugin add kotlin
+asdf install kotlin latest
+asdf global kotlin latest
+asdf shell kotlin latest
+
+# Install Scala
+asdf plugin add scala
+asdf install scala latest
+asdf global scala latest
+asdf shell scala latest
+
+# Install Dart
+asdf plugin add dart
+asdf install dart latest
+asdf global dart latest
+asdf shell dart latest
+
+# Install Flutter
+asdf plugin add flutter
+asdf install flutter latest
+asdf global flutter latest
+asdf shell flutter latest
