9af780a304a89b4ac933368aed29215a0abf7622
architectureman <111078057+architectureman@users.noreply.github.com>
2024-10-15 05:15:03 +0700

    Create install_terminal_apps.sh

---
+++ scripts/install_terminal_apps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install spotify-tui
+cargo install spotify-tui
+
+# Install youtube-tui
+cargo install youtube-tui
+
+# Install dblab-bin
+cargo install dblab
+
+# Install gobang-bin
+cargo install gobang
+
+# Install tgt
+cargo install tgt
+
