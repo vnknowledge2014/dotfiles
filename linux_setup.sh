82d110de7539afbb7a05470ac5c5e58436c399b0
architectureman <111078057+architectureman@users.noreply.github.com>
2024-10-15 12:19:06 +0700

    Update linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Add HOMEBREW to Path
+# Add Homebrew to PATH
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
