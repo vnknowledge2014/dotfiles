4336f591a6d714fc26d6e63471dc8f6a2048f8ed
architectureman <111078057+architectureman@users.noreply.github.com>
2024-10-15 12:15:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 # ZSH - STARSHIP - NEOVIM
 brew install zsh starship neovim
 
